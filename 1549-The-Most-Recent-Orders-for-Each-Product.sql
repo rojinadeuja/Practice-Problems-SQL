f961e64bc4fc03c3157bc3937c7f87352fdaec28
rojinadeuja <rojinadeuja33g@gmail.com>
2020-11-11 11:03:20 -0600

    Add query

--- 1549-The-Most-Recent-Orders-for-Each-Product.sql
+++ 1549-The-Most-Recent-Orders-for-Each-Product.sql
@@ -5,3 +5,12 @@ Find most recent order(s) of each product.
 MAX(Date) won't work because we need MAX by grouping so use RANK() OVER (PARTITION..) 
 This gives multiple max rows as well.
 */
+
+SELECT p.product_name, p.product_id, o.order_id, MAX(o.order_date) as order_date FROM
+Products p JOIN
+(SELECT *, RANK() OVER(PARTITION BY product_id ORDER BY order_date DESC) as ranking
+FROM Orders) o
+ON o.product_id = p.product_id
+WHERE o.ranking = 1
+GROUP BY o.product_id, o.order_id
+ORDER BY product_name, product_id, order_id;
